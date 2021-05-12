c01d05d669399420f8c69354aee322e1fa298af0
birdshiu <qaz0975417436@gmail.com>
2021-05-12 15:01:30 +0800

    add jdbc, but can't work. android add loginactivity

--- JavaServer/src/JavaServer/JavaServer.java
+++ JavaServer/src/JavaServer/JavaServer.java
@@ -1,9 +1,17 @@
 package JavaServer;
 
+import java.sql.*;
 public class JavaServer {
 
 	public static void main(String[] args) {
-		System.out.println("yes");
+		try {
+			Class.forName("com.mysql.jdbc.Driver");     //載入MYSQL JDBC驅動程式   
+			//Class.forName("org.gjt.mm.mysql.Driver");
+			System.out.println("Success loading Mysql Driver!");
+			} catch (Exception e) {
+			System.out.print("Error loading Mysql Driver!");
+			e.printStackTrace();
+			}
 	}
 
 }
---
+++ WhosCall/app/src/main/java/com/example/whoscall/LoginActivity.java
@@ -0,0 +1,4 @@
+package com.example.whoscall;
+
+public class LoginActivity {
+}
