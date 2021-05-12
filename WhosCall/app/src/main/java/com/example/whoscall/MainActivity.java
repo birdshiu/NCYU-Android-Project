be57450fd79692a27a8b2b73472a35cc71dca8c8
birdshiu <qaz0975417436@gmail.com>
2021-05-12 16:04:14 +0800

    LoginActivity layout OK

--- WhosCall/app/src/main/java/com/example/whoscall/MainActivity.java
+++ WhosCall/app/src/main/java/com/example/whoscall/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.whoscall;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CallLog;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txt=findViewById(R.id.txt);
+
+        Uri allCalls = Uri.parse("content://call_log/calls");
+        //Cursor c = managedQuery(CallLog.Calls.CONTENT_URI, null, null, null, null);
+
+        /*String result="";
+
+        String num= c.getString(c.getColumnIndex(CallLog.Calls.NUMBER));// for  number
+        String name= c.getString(c.getColumnIndex(CallLog.Calls.CACHED_NAME));// for name
+        String duration = c.getString(c.getColumnIndex(CallLog.Calls.DURATION));// for duration
+        int type = Integer.parseInt(c.getString(c.getColumnIndex(CallLog.Calls.TYPE)));// for call type, Incoming or out going.
+
+        result+=num+", "+name+", "+duration+", "+type;
+        txt.setText(result);*/
     }
 }
